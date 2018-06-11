51bc795c9b07487c87dc7a52653aae46d2ae14a8
Anna Ira <annairahurnaus@mytum.de>
2018-06-11 19:42:50 +0200

    refactor

--- src/main/java/de/codefor/opacapi/Application.java
+++ src/main/java/de/codefor/opacapi/Application.java
@@ -1,12 +1,13 @@
 package de.codefor.opacapi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        run(Application.class, args);
     }
 }
